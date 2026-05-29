da6ca27d5be9237decb6734f592139a6dc662175
edharairapetov-source <edhar.airapetov@hneu.net>
2026-05-29 14:10:45 +0300

    Refactor test_views.py for improved readability

--- test_views.py
+++ test_views.py
@@ -5,30 +5,30 @@ from django.contrib.auth.models import User
 class MlTradingViewTestCase(TestCase):
 
     def setUp(self):
-        # Создаем тестового пользователя
+        
         self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.url = reverse('python_console')  # Путь к твоей странице анализа
+        self.url = reverse('python_console')  
 
     def test_ml_trading_view_status_code(self):
-        # Проверка, что страница доступна (200 OK)
+       
         self.client.login(username='testuser', password='testpassword')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
 
     def test_ml_trading_view_template(self):
-        # Проверка, что используется правильный шаблон
+      
         self.client.login(username='testuser', password='testpassword')
         response = self.client.get(self.url)
-        self.assertTemplateUsed(response, 'ml_trading.html')  # Убедись, что указываешь правильный путь
+        self.assertTemplateUsed(response, 'ml_trading.html') 
 
     def test_ml_trading_view_context(self):
-        # Проверка наличия нужных данных в контексте
+       
         self.client.login(username='testuser', password='testpassword')
         response = self.client.get(self.url)
-        self.assertIn('results', response.context)  # Убедись, что данные действительно передаются в контекст
-        self.assertIsInstance(response.context['results'], dict)  # Проверка типа данных в контексте
+        self.assertIn('results', response.context) 
+        self.assertIsInstance(response.context['results'], dict)  
 
     def test_ml_trading_view_redirect_if_not_logged_in(self):
-        # Проверка перенаправления для пользователей, не вошедших в систему
+      
         response = self.client.get(self.url)
         self.assertRedirects(response, '/login/?next=/ml-trading/')
